a889ceced8f87bdb2a3c179e231b7c6ff6a4be37
tareque-bin-habib <shad.tbh@gmail.com>
2022-11-19 23:18:47 +0600

    map products

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-const Product = () => {
+function Product({ product }) {
+    const { img, name, price, details } = product
     return (
-        <div>
-            <h1>Productss</h1>
+        <div className='product'>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={img} />
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text>
+                        <p>{details}</p>
+                        <p>Price: <small>{price}</small>$</p>
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+
+            </Card>
         </div>
     );
-};
+}
 
 export default Product;
